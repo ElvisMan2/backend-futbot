61f70a63ddee9cd043aab1205a67865f55dca0cb
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2025-10-25 09:59:33 -0500

    cors config

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import reservaRoutes from './routes/reservaRoutes.js';
 
 const app = express();
+
+// Permitir CORS desde cualquier origen
+app.use(cors());
+
+// Para parsear JSON
 app.use(express.json());
 
+// Rutas
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/reservas', reservaRoutes);
 
